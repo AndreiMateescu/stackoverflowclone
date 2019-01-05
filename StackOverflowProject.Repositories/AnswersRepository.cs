8722a7a04629e4a416279133b436a93798a160e6
StreetHacker007 <andrei.mateescu@icloud.com>
2019-01-05 17:22:45 +0200

    Add Answer and Vote Repositories

---
+++ StackOverflowProject.Repositories/AnswersRepository.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StackOverflowProject.DomainModels;
+
+namespace StackOverflowProject.Repositories
+{
+    public interface IAnswersRepository
+    {
+        void InsertAnswer(Answer answer);
+        void UpdateAnswer(Answer answer);
+        void UpdateAnswerVotesCount(int answerID, int userID, int value);
+        void DeleteAnswer(int answerID);
+        List<Answer> GetAnswersByQuestionID(int questionID);
+        List<Answer> GetAnswersByAnswerID(int answerID);
+    }
+
+    public class AnswersRepository : IAnswersRepository
+    {
+        StackOverflowDatabaseDbContext db;
+        IQuestionRepository qr;
+        IVotesRepository vr;
+
+        public AnswersRepository()
+        {
+            db = new StackOverflowDatabaseDbContext();
+            qr = new QuestionsRepository();
+            vr = new VotesRepository();
+        }
+
+        public void DeleteAnswer(int answerID)
+        {
+            var ans = db.Answers.Where(temp => temp.AnswerID == answerID).FirstOrDefault();
+            if(ans != null)
+            {
+                db.Answers.Remove(ans);
+                db.SaveChanges();
+                qr.UpdateQuestionAnswersCount(ans.QuestionID, -1);
+            }
+        }
+
+        public List<Answer> GetAnswersByAnswerID(int answerID)
+        {
+            return db.Answers.Where(temp => temp.AnswerID == answerID).ToList();
+        }
+
+        public List<Answer> GetAnswersByQuestionID(int questionID)
+        {
+            return db.Answers.Where(temp => temp.QuestionID == questionID).ToList();
+        }
+
+        public void InsertAnswer(Answer answer)
+        {
+            db.Answers.Add(answer);
+            db.SaveChanges();
+            qr.UpdateQuestionAnswersCount(answer.QuestionID, 1);
+        }
+
+        public void UpdateAnswer(Answer answer)
+        {
+            var ans = db.Answers.Where(temp => temp.AnswerID == answer.AnswerID).FirstOrDefault();
+            if(ans != null)
+            {
+                ans.AnswerText = answer.AnswerText;
+                db.SaveChanges();
+            }
+        }
+
+        public void UpdateAnswerVotesCount(int answerID, int userID, int value)
+        {
+            var ans = db.Answers.Where(temp => temp.AnswerID == answerID).FirstOrDefault();
+            if (ans != null)
+            {
+                ans.VotesCount += value;
+                db.SaveChanges();
+                qr.UpdateQuestionVotesCount(ans.QuestionID, value);
+                vr.UpdateVote(answerID, userID, value);
+            }
+        }
+    }
+}
---
+++ StackOverflowProject.Repositories/VotesRepository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StackOverflowProject.DomainModels;
+
+namespace StackOverflowProject.Repositories
+{
+    public interface IVotesRepository
+    {
+        void UpdateVote(int answerID, int userID, int value);
+    }
+
+    public class VotesRepository : IVotesRepository
+    {
+        StackOverflowDatabaseDbContext db;
+
+        public VotesRepository()
+        {
+            db = new StackOverflowDatabaseDbContext();
+        }
+
+        public void UpdateVote(int answerID, int userID, int value)
+        {
+            int updateValue;
+            if(value > 0)
+            {
+                updateValue = 1;
+            }
+            else if(value < 0)
+            {
+                updateValue = -1;
+            }
+            else
+            {
+                updateValue = 0;
+            }
+
+            Vote vote = db.Votes.Where(temp => temp.AnswerID == answerID && temp.UserID == userID).FirstOrDefault();
+            if(vote != null)
+            {
+                vote.VoteValue = updateValue;
+            }
+            else
+            {
+                Vote newVote = new Vote() { AnswerID = answerID, UserID = userID, VoteValue = updateValue };
+                db.Votes.Add(newVote);
+            }
+            db.SaveChanges();
+        }
+    }
+}
