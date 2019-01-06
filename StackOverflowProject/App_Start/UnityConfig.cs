94810a8f841db4ffe80d227b8952ea406adb55cc
StreetHacker007 <andrei.mateescu@icloud.com>
2019-01-07 00:33:37 +0200

    Add dependency injection

---
+++ StackOverflowProject/App_Start/UnityConfig.cs
@@ -0,0 +1,22 @@
+using System.Web.Http;
+using Unity;
+using Unity.WebApi;
+using Unity.Mvc5;
+using StackOverflowProject.ServiceLayer;
+using System.Web.Mvc;
+
+namespace StackOverflowProject
+{
+    public static class UnityConfig
+    {
+        public static void RegisterComponents()
+        {
+			var container = new UnityContainer();
+
+            container.RegisterType<IQuestionsService, QuestionsService>();
+
+            DependencyResolver.SetResolver(new Unity.Mvc5.UnityDependencyResolver(container));
+            GlobalConfiguration.Configuration.DependencyResolver = new Unity.WebApi.UnityDependencyResolver(container);
+        }
+    }
+}
--- StackOverflowProject/Controllers/HomeController.cs
+++ StackOverflowProject/Controllers/HomeController.cs
@@ -3,15 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using StackOverflowProject.ServiceLayer;
 
 namespace StackOverflowProject.Controllers
 {
     public class HomeController : Controller
     {
+        IQuestionsService qs;
+
+        public HomeController(IQuestionsService qs)
+        {
+            this.qs = qs;
+        }
+
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            return View(this.qs.GetQuestions().Take(10).ToList());
         }
     }
 }
