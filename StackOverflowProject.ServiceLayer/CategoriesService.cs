f4f9f25accf77863b845884f63b5d54a14fa373d
StreetHacker007 <andrei.mateescu@icloud.com>
2019-01-06 16:45:25 +0200

    Add CategoriesService

---
+++ StackOverflowProject.ServiceLayer/CategoriesService.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StackOverflowProject.DomainModels;
+using StackOverflowProject.Repositories;
+using StackOverflowProject.ViewModels;
+using AutoMapper;
+using AutoMapper.Configuration;
+
+namespace StackOverflowProject.ServiceLayer
+{
+    public interface ICategoriesService
+    {
+        void InsertCategory(CategoryViewModel cvm);
+        void UpdateCategory(CategoryViewModel cvm);
+        void DeleteCategory(int cid);
+        List<CategoryViewModel> GetCategories();
+        CategoryViewModel GetCategoryByCategoryID(int categoryID);
+    }
+
+    public class CategoriesService : ICategoriesService
+    {
+        ICategoriesRepository cr;
+
+        public CategoriesService()
+        {
+            cr = new CategoriesRepository();
+        }
+
+        public void DeleteCategory(int cid)
+        {
+            cr.DeleteCategory(cid);
+        }
+
+        public List<CategoryViewModel> GetCategories()
+        {
+            List<Category> c = cr.GetCategories();
+            var config = new MapperConfiguration(cfg => { cfg.CreateMap<Category, CategoryViewModel> (); cfg.IgnoreUnmapped(); });
+            IMapper mapper = config.CreateMapper();
+            List<CategoryViewModel> cvm = mapper.Map<List<Category>, List<CategoryViewModel>>(c);
+            return cvm;
+        }
+
+        public CategoryViewModel GetCategoryByCategoryID(int categoryID)
+        {
+            Category c = cr.GetCategoryByCategoryID(categoryID).FirstOrDefault();
+            CategoryViewModel cvm = null;
+            if (c != null)
+            {
+                var config = new MapperConfiguration(cfg => { cfg.CreateMap<Category, CategoryViewModel>(); cfg.IgnoreUnmapped(); });
+                IMapper mapper = config.CreateMapper();
+                cvm = mapper.Map<Category, CategoryViewModel>(c);
+            }
+            return cvm;
+        }
+
+        public void InsertCategory(CategoryViewModel cvm)
+        {
+            var config = new MapperConfiguration(cfg => { cfg.CreateMap<CategoryViewModel, Category>(); cfg.IgnoreUnmapped(); });
+            IMapper mapper = config.CreateMapper();
+            Category c = mapper.Map<CategoryViewModel, Category>(cvm);
+            cr.InsertCategory(c);
+        }
+
+        public void UpdateCategory(CategoryViewModel cvm)
+        {
+            var config = new MapperConfiguration(cfg => { cfg.CreateMap<CategoryViewModel, Category>(); cfg.IgnoreUnmapped(); });
+            IMapper mapper = config.CreateMapper();
+            Category c = mapper.Map<CategoryViewModel, Category>(cvm);
+            cr.UpdateCategory(c);
+        }
+    }
+}
