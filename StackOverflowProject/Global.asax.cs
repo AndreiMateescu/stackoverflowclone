cfab88de50d9a18d26cbd4141c8227232ac9e6c7
StreetHacker007 <andrei.mateescu@icloud.com>
2019-01-06 17:30:11 +0200

    Add bundles and minification

--- StackOverflowProject/Global.asax.cs
+++ StackOverflowProject/Global.asax.cs
@@ -7,6 +7,8 @@ using System.Web.Routing;
 using System.Web.Security;
 using System.Web.SessionState;
 using System.Web.Http;
+using System.Web.Optimization;
+using StackOverflowProject.App_Start;
 
 namespace StackOverflowProject
 {
@@ -16,6 +18,7 @@ namespace StackOverflowProject
         {
             // Code that runs on application startup
             AreaRegistration.RegisterAllAreas();
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
             GlobalConfiguration.Configure(WebApiConfig.Register);
             RouteConfig.RegisterRoutes(RouteTable.Routes);            
         }
