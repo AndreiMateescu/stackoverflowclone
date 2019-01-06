f85f906cc80cb84eab724ea5ca460f18418cb7a6
StreetHacker007 <andrei.mateescu@icloud.com>
2019-01-06 14:29:21 +0200

    Add remaining view models

---
+++ StackOverflowProject.ViewModels/EditAnswerViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackOverflowProject.ViewModels
+{
+    public class EditAnswerViewModel
+    {
+        [Required]
+        public int AnswerID { get; set; }
+
+        [Required]
+        public string AnswerText { get; set; }
+
+        [Required]
+        public DateTime AnswerDateAndTime { get; set; }
+
+        [Required]
+        public int UserID { get; set; }
+        public int QuestionID { get; set; }
+
+        [Required]
+        public int VotesCount { get; set; }
+
+        public virtual QuestionViewModel Question { get; set; }
+    }
+}
---
+++ StackOverflowProject.ViewModels/VoteViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackOverflowProject.ViewModels
+{
+    public class VoteViewModel
+    {
+        [Required]
+        public int VoteID { get; set; }
+
+        [Required]
+        public int UserID { get; set; }
+
+        [Required]
+        public int AnswerID { get; set; }
+
+        [Required]
+        public int VoteValue { get; set; }
+    }
+}
