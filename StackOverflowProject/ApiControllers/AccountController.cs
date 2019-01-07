7dcdfded66f95e1803d9984f16587e9bbcdcf682
StreetHacker007 <andrei.mateescu@icloud.com>
2019-01-07 23:08:33 +0200

    Add WebApi for checking if an email exist. Call this with ajax

---
+++ StackOverflowProject/ApiControllers/AccountController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using StackOverflowProject.ServiceLayer;
+using StackOverflowProject.ViewModels;
+
+namespace StackOverflowProject.ApiControllers
+{
+    public class AccountController : ApiController
+    {
+        IUsersService us;
+
+        public AccountController(IUsersService us)
+        {
+            this.us = us;
+        }
+
+        public string Get(string email)
+        {
+            if (this.us.GetUsersByEmail(email) != null)
+            {
+                return "Found";
+            }
+            return "Not found";
+        }
+    }
+}
